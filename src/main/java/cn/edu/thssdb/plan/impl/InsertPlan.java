a9896dbf1da203bf140dfabc1c67b0607d85f127
adfwer233 <ba123698745@126.com>
2023-05-22 19:10:52 +0800

    feat: add insert plan

---
+++ src/main/java/cn/edu/thssdb/plan/impl/InsertPlan.java
@@ -0,0 +1,31 @@
+package cn.edu.thssdb.plan.impl;
+
+import cn.edu.thssdb.plan.LogicalPlan;
+import cn.edu.thssdb.schema.Entry;
+
+import java.util.List;
+
+public class InsertPlan extends LogicalPlan {
+    private String tableName;
+    private List<String> attributeNameList;
+    private List<List<String>> entryValueList;
+
+    public InsertPlan(String tableName, List<String> attributeNameList, List<List<String>> entryValueList) {
+        super(LogicalPlanType.INSERT);
+        this.tableName = tableName;
+        this.attributeNameList = attributeNameList;
+        this.entryValueList = entryValueList;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public List<List<String>> getEntryValueList() {
+        return entryValueList;
+    }
+
+    public List<String> getAttributeNameList() {
+        return attributeNameList;
+    }
+}
