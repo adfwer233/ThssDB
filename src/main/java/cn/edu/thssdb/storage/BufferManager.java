cc37159580e25cd345f9ee245d9b0b7ad2fc3e1e
adfwer233 <ba123698745@126.com>
2023-05-28 00:11:37 +0800

    feat: add buffer manager

--- src/main/java/cn/edu/thssdb/storage/BufferManager.java
+++ src/main/java/cn/edu/thssdb/storage/BufferManager.java
@@ -3,9 +3,10 @@ package cn.edu.thssdb.storage;
 import cn.edu.thssdb.schema.Row;
 import cn.edu.thssdb.schema.Table;
 
-import java.io.File;
+import java.io.*;
 import java.nio.Buffer;
 import java.util.ArrayList;
+import java.util.Deque;
 
 public class BufferManager {
   public final static Integer BufferSize = 10;
@@ -14,7 +15,6 @@ public class BufferManager {
   private String tableDir;
 
   ArrayList<ArrayList<Row>> buffer = new ArrayList<>();
-
   ArrayList<Integer> bufferPageIndex = new ArrayList<>();
 
   ArrayList<Boolean> writeFlag = new ArrayList<>();
@@ -23,16 +23,147 @@ public class BufferManager {
     tableDir = table.getTableFolderPath();
   }
 
-  public void writePage(Integer PageIndex, ArrayList<Row> data) {
+  private void dropPage() {
 
+    Boolean pageWriteFlag = writeFlag.get(0);
+    writeFlag.remove(0);
+    ArrayList<Row> page = buffer.get(0);
+    buffer.remove(0);
+    Integer index = bufferPageIndex.get(0);
+    bufferPageIndex.remove(0);
+
+    /*
+     * Just Drop the oldest page
+     * If the page to drop has no write flag, do nothing
+     * TODO: add page selection such as LRU
+     * */
+    if (!pageWriteFlag)
+      return;
+
+    File folder = new File(tableDir);
+    if (!folder.exists()) {
+      folder.mkdirs();
+    }
+
+    File pagePath = new File(getPagePath(index));
+    if (!pagePath.exists()) {
+      try {
+        pagePath.createNewFile();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    try {
+      FileOutputStream fileOutputStream = new FileOutputStream(pagePath);
+      BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+      ObjectOutputStream objectOutputStream = new ObjectOutputStream(bufferedOutputStream);
+
+      for (Row row : page) {
+        objectOutputStream.writeObject(row);
+      }
+
+      objectOutputStream.close();
+      bufferedOutputStream.flush();
+      bufferedOutputStream.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
-  public void readPage(Integer pageIndex) {
+  private ArrayList<Row> getPage(Integer pageIndex) {
+    File folder = new File(tableDir);
+    if (!folder.exists()) {
+      folder.mkdirs();
+    }
 
+    File pagePath = new File(getPagePath(pageIndex));
+    if (!pagePath.exists()) {
+      try {
+        pagePath.createNewFile();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    ArrayList<Row> res = new ArrayList<>();
+    try {
+      FileInputStream fileInputStream = new FileInputStream(pagePath);
+      BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+      ObjectInputStream objectInputStream = new ObjectInputStream(bufferedInputStream);
+
+      Object inputObject;
+      while (fileInputStream.available() > 0) {
+        inputObject = objectInputStream.readObject();
+        res.add((Row) inputObject);
+      }
+
+      bufferedInputStream.close();
+      fileInputStream.close();
+    } catch (IOException | ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    return res;
+  }
+
+  public void writePage(Integer pageIndex, ArrayList<Row> data) {
+
+    /*
+    * If page in buffer
+    * replace the current buffered page
+    * set the dirty flag
+    * */
+    if (bufferPageIndex.contains(pageIndex)) {
+      buffer.set(bufferPageIndex.indexOf(pageIndex), data);
+      writeFlag.set(bufferPageIndex.indexOf(pageIndex), true);
+      return;
+    }
+
+    /*
+    * If buffer is not full
+    * add the page to buffer
+    * */
+    if (buffer.size() <= BufferSize) {
+      buffer.add(data);
+      bufferPageIndex.add(pageIndex);
+      writeFlag.add(false);
+      return;
+    }
+
+    dropPage();
+  }
+
+  public ArrayList<Row> readPage(Integer pageIndex) {
+    /*
+    * If page in buffer, just return it
+    * */
+    if (bufferPageIndex.indexOf(pageIndex) >= 0) {
+      return buffer.get(bufferPageIndex.indexOf(pageIndex));
+    }
+
+    // get page from file system
+    ArrayList<Row> page = readPage(pageIndex);
+
+    /*
+    * read page and put it into buffer.
+    * if the buffer is full, drop the first page.
+    * */
+
+    buffer.add(page);
+    bufferPageIndex.add(pageIndex);
+    writeFlag.add(false);
+
+    if (buffer.size() > BufferSize)
+      dropPage();
+
+    return page;
   }
 
   public void flush() {
-
+    while (!buffer.isEmpty()) {
+      dropPage();
+    }
   }
 
   private String getPagePath(Integer page) {
